15130d1ae13efd43a3aae0433b26bffde0863942
rahulsd380 <rahulsd380@gmail.com>
2026-01-29 14:23:14 +0600

    Completed services page

---
+++ src/app/(commonLayout)/services/page.tsx
@@ -0,0 +1,13 @@
+import NumberedGridSection from "@/components/Reusable/NumberedGridSection/NumberedGridSection";
+import ServicesHero from "@/components/ServicesPage/ServicesHero/ServicesHero";
+
+const Services = () => {
+  return (
+    <div>
+      <ServicesHero />
+      <NumberedGridSection heading="Our Practice Areas" />
+    </div>
+  );
+};
+
+export default Services;
--- src/components/Reusable/Heading/Heading.tsx
+++ src/components/Reusable/Heading/Heading.tsx
@@ -1,11 +1,18 @@
-
-const Heading = ({heading, description} : {heading: string, description: string}) => {
+const Heading = ({
+  heading,
+  description,
+}: {
+  heading: string;
+  description?: string;
+}) => {
   return (
     <div>
       <h3 className="title font-neue-montreal">{heading}</h3>
-      <p className="text-lg text-neutral-15 mt-3 font-work-sans">
-        {description}
-      </p>
+      {description && (
+        <p className="text-lg text-neutral-15 mt-3 font-work-sans">
+          {description}
+        </p>
+      )}
     </div>
   );
 };
